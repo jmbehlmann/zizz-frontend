a013a70f0f5105a49ec1f3c793d8424b9e6a0b64
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-15 12:48:10 -0600

    added blog posts, organized App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
   )
 }
 
-export default App
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>Make a new post</h1>
+      <form>
+        <p>Title: <input type="text" /> </p>
+        <p>Autor: <input type="text" /> </p>
+        <p>Text: <input type="text" /> </p>
+      </form>
+    </div>
+)
+}
+
+function PostsIndex() {
+  return(
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <div className="posts">
+        <h2>Post #1</h2>
+        <img src="https://cdn.britannica.com/13/100513-050-1DB2C84D/Tractor-loader.jpg" />
+        <p>author: James</p>
+        <p>texty text text</p>
+        <button>read post</button>
+      </div>
+      <div className="posts">
+        <h2>Post #2</h2>
+        <img src="https://moma.org/wp/inside_out/wp-content/uploads/2013/10/The-Apparition.jpg" />
+        <p>author: James</p>
+        <p>nice post right here!</p>
+        <button>read post</button>
+      </div>
+        <div className="posts">
+        <h2>Post #3</h2>
+        <img src="https://mediaproxy.salon.com/width/1200/https://media.salon.com/2013/07/2001_monolith.jpg" />
+        <p>author: James</p>
+        <p>nice post right there!</p>
+        <button>read post</button>
+      </div>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2078</p>
+    </footer>
+  )
+}
+
+function Content() {
+  return(
+  <div>
+    <PostsNew />
+    <PostsIndex />
+  </div>
+  )
+}
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
