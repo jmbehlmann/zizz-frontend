ea28530f1a6ec4c217d83f8ed0059e90974adf0e
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-15 13:37:57 -0600

    reformatted app.jsx to be dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -21,31 +21,20 @@ function PostsNew() {
 )
 }
 
-function PostsIndex() {
+function PostsIndex(props) {
+  console.log(props.posts)
+
   return(
     <div id="posts-index">
       <h1>All posts</h1>
-      <div className="posts">
-        <h2>Post #1</h2>
-        <img src="https://cdn.britannica.com/13/100513-050-1DB2C84D/Tractor-loader.jpg" />
-        <p>author: James</p>
-        <p>texty text text</p>
-        <button>read post</button>
-      </div>
-      <div className="posts">
-        <h2>Post #2</h2>
-        <img src="https://moma.org/wp/inside_out/wp-content/uploads/2013/10/The-Apparition.jpg" />
-        <p>author: James</p>
-        <p>nice post right here!</p>
-        <button>read post</button>
-      </div>
-        <div className="posts">
-        <h2>Post #3</h2>
-        <img src="https://mediaproxy.salon.com/width/1200/https://media.salon.com/2013/07/2001_monolith.jpg" />
-        <p>author: James</p>
-        <p>nice post right there!</p>
-        <button>read post</button>
-      </div>
+      {props.posts.map(post => (
+        <div key={post.id}>
+          <h2>title: {post.title}</h2>
+          <p>author: {post.author}</p>
+          <img src={post.image_url} />
+          <p>{post.text}</p>
+        </div>
+      ))}
     </div>
   )
 }
@@ -59,10 +48,33 @@ function Footer() {
 }
 
 function Content() {
+  let posts = [
+    {
+      id: 1,
+      title: "Post #1",
+      author: "James",
+      image_url: "https://cdn.britannica.com/13/100513-050-1DB2C84D/Tractor-loader.jpg",
+      text: "texty text text"
+    },
+    {
+      id: 2,
+      title: "Post #1",
+      author: "James",
+      image_url: "https://moma.org/wp/inside_out/wp-content/uploads/2013/10/The-Apparition.jpg",
+      text: "nice post right there!"
+    },
+    {
+      id: 3,
+      title: "Post #3",
+      author: "James",
+      image_url: "https://mediaproxy.salon.com/width/1200/https://media.salon.com/2013/07/2001_monolith.jpg",
+      text: "nice post right here!"
+    }
+  ]
   return(
   <div>
     <PostsNew />
-    <PostsIndex />
+    <PostsIndex  posts={posts} />
   </div>
   )
 }
