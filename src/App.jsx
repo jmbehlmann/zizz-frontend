0ee40b20bbdd68c77baf45ceffaae14af2f4efa9
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-18 12:48:18 -0600

    setup to get data from rails backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import './App.css'
 
+
 import { Content } from "./Content"
 import { Footer } from "./Footer"
 import { Header } from "./Header";
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,34 +1,27 @@
 import { PostsNew } from "./PostsNew";
 import { PostsIndex } from "./PostsIndex";
+import axios from "axios";
+
 
 export function Content() {
-  let posts = [
-    {
-      id: 1,
-      title: "Tractors",
-      author: "James",
-      image_url: "https://cdn.britannica.com/13/100513-050-1DB2C84D/Tractor-loader.jpg",
-      text: "texty text text"
-    },
-    {
-      id: 2,
-      title: "Words",
-      author: "James",
-      image_url: "https://moma.org/wp/inside_out/wp-content/uploads/2013/10/The-Apparition.jpg",
-      text: "nice post right there!"
-    },
-    {
-      id: 3,
-      title: "Monoliths",
-      author: "James",
-      image_url: "https://mediaproxy.salon.com/width/1200/https://media.salon.com/2013/07/2001_monolith.jpg",
-      text: "nice post right here!"
-    }
-  ];
+  let posts = [];
+
+  const handleIndexPosts = () => {
+    console.log("in handle index posts");
+    axios.get('http://localhost:3000/posts.json').then(response => {
+      console.log(response.data)
+    })
+  }
+
   return (
     <div>
       <PostsIndex posts={posts} />
       <PostsNew />
+      <button onClick={handleIndexPosts}>Get posts from the api </button>
     </div>
+
+
   );
+
+
 }
